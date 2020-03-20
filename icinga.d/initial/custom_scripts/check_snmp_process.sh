ccd873497e7978da3bf1ba9541664265113722de
root <root@ccts-mon.chpc.utah.edu>
2020-03-20 10:21:20 -0600

    updated

--- icinga.d/initial/custom_scripts/check_snmp_process.sh
+++ icinga.d/initial/custom_scripts/check_snmp_process.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Andrew Golightly
+# August 2019
+# Checks to see if a certain process is running via snmp
+
 # Used for pretty formatting of results. 
 RED='\033[0;31m'
 GREEN='\033[1;32m'
